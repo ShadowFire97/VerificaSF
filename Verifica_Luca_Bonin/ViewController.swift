42af04b99a3f9ac4f63edc5a661e4e5194606ad6
LeoLuca97 <llb.developer.97@gmail.com>
2018-11-20 09:18:36 +0100

    Initial Commit

---
+++ Verifica_Luca_Bonin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Verifica_Luca_Bonin
+//
+//  Created by Leo Luca Bonin on 20/11/2018.
+//  Copyright © 2018 developer.llb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Verifica_Luca_BoninTests/Verifica_Luca_BoninTests.swift
@@ -0,0 +1,34 @@
+//
+//  Verifica_Luca_BoninTests.swift
+//  Verifica_Luca_BoninTests
+//
+//  Created by Leo Luca Bonin on 20/11/2018.
+//  Copyright © 2018 developer.llb. All rights reserved.
+//
+
+import XCTest
+@testable import Verifica_Luca_Bonin
+
+class Verifica_Luca_BoninTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
